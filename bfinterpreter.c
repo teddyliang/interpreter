a34e61a51a082471b0ad1f1c35311159e07fcd10
teddyliang <teddytduoliang@gmail.com>
2020-04-02 14:12:52 -0700

    file stuff

--- bfinterpreter.c
+++ bfinterpreter.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+
+int isFileExists(const char *path)
+{
+	printf("THE PATH::::::%s\n", path);
+    FILE *filePointer = fopen(path, "r");
+    if (filePointer == NULL)
+    	printf("peeeeee\n");
+        return 0;
+    fclose(filePointer);
+
+    return 1;
+}
 
 int main()
 {
-	
-	return 0;
+	char filepath[150];
+	printf("Enter the file path:");
+	fgets(filepath, 150, stdin);
+	printf("%s\n", filepath);
+
+	if (isFileExists(filepath))
+    {
+        printf("FILE EXIST!\n");
+    }
+    else
+    {
+        printf("FILE NO EXIST\n");
+    }
 }
+
